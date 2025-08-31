4b720167d5312591586fe267977b2e06cbab7c83
MD.NAIMUR RAHMAN <naimur655@gmail.com>
2025-08-31 09:06:40 +0600

    initial userdashboard

---
+++ view/student/s_dashboard.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <script src="Script.js" defer></script>
+        <link rel="stylesheet" href="style.css">
+
+    </head>
+    <body>
+
+    <a href="#" class="logout-btn">Logout</a>
+
+    <div class="dashboard-container">
+     
+        <h3> Student Dashboard</h3>
+        <ul>
+          <li><a href="courses.php">View Available Courses</a></li>
+          <li><a href="enroll.php">Enroll in a Course</a></li>
+          <li><a href="assignments.php">Submit Assignment/Exam</a></li>
+          <li><a href="grades.php">View Grades & Progress</a></li>
+        </ul>
+    </body>
+</head>
